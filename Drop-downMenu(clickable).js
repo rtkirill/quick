e6d390ea8f454be8b09acbe6e88be9fed3968f22
Kirill <elpuffru@gmail.com>
2017-04-28 12:42:43 +0300

    Drop-down menu (clickable) v2

--- Drop-downMenu(clickable).js
+++ Drop-downMenu(clickable).js
@@ -1,12 +1,17 @@
-$(document).on('click', '.gh-menu-link-arrow', function (e) {
-    var container = $(this).next();
+//Выпадающее меню по клику.
+//Закрытие осуществляется по повторному клику.
+//----------------------------------
+//Заменить MENU_LINK и DROP-DOWN_DIV
+
+$(document).on('click', 'MENU_LINK', function (e) {
+    var container = $(this).next(); //div идет после link внутри одного родителя
     e.preventDefault();
-    if(container.is(':visible')) {
+    if(container.is(':visible')) { //закрытие пункта по повторному нажатию
         container.hide();
-    } else if(!(container.is(':visible')) && $('.gh-sub-menu-cell').is(':visible')) {
-        $('.gh-sub-menu-cell').hide();
+    } else if(!(container.is(':visible')) && $('DROP-DOWN_DIV').is(':visible')) { //закрытие всех остальных
+        $('DROP-DOWN_DIV').hide();
         container.show();
-    } else {
+    } else { //открытие нажатого пункта
         container.show();
     }
 })
